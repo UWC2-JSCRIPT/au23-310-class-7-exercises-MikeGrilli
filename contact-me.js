d4c08c9e970441aed90f852607d03cbe085ba6dd
Mike Grilli <grillos80@gmail.com>
2023-11-26 11:12:19 -0800

    updated Assignment

--- contact-me.js
+++ contact-me.js
@@ -1,1 +1,26 @@
 // TODO
+const connectForm = document.getElementById('connect-form')
+const fname = document.getElementById('fname')
+const lname = document.getElementById('lname')
+const email = document.getElementById('email')
+const reasonForContacting = document.getElementById('reason-for-contacting')
+const jobTitle = document.getElementById('job-title')
+const website = document.getElementById('website')
+const codingLanguage = document.getElementById('coding-language')
+const message = document.getElementById('message')
+
+
+
+const validLength = (input, min) => {
+	if (input.value.trim().length >= min) { 
+		input.parentElement.classList.remove("invalid");
+	} else {
+		input.parentElement.classList.add("valid");
+	}
+}
+
+connectForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    validLength(fname, 3)
+})
+
