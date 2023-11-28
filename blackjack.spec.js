e15623d7eca7028e5842280f0a0e74862a777232
Mike Grilli <grrillos80gmail.com>
2023-11-28 15:25:40 -0800

    Updated Assignment

--- blackjack.spec.js
+++ blackjack.spec.js
@@ -7,6 +7,49 @@ describe('Test for the blackjack game', () => {
         ]
         const result = calcPoints(hand)
         expect(result.total).toEqual(13)
-        expect(result.isSoft).toEqual(false)
+
     }) 
 })
+
+describe('Test for the blackjack game', () => {
+    it('Should calculate the score of a hand', () => {
+        const hand = [
+            {displayVal: '6', val :6, suit: 'hearts'},
+            {displayVal: '7', val :7, suit: 'hearts'},
+            {displayVal: '7', val :5, suit: 'hearts'}
+
+        ]
+        const result = calcPoints(hand)
+        expect(result.total).toEqual(18)
+    
+    }) 
+})
+
+describe('Test for the blackjack game', () => {
+    it('Should calculate if hand is soft', () => {
+        const hand = [
+            {displayVal: 'Ace', val :1, suit: 'hearts'},
+            {displayVal: '7', val :7, suit: 'hearts'}
+        ]
+        const result = calcPoints(hand).isSoft
+        expect(result).toBe(true)
+    }) 
+})
+
+
+describe('Test to see if dealer should draw', () => {
+    it('Should determine if dealer should draw', () => {
+        const hand = [
+            {displayVal: '10', val :10, suit: 'hearts'},
+            {displayVal: '8', val :8, suit: 'hearts'}
+        ]
+        const result = dealerShouldDraw(hand)
+        expect(result).toBe(false)
+    }) 
+})
+
+
+
+
+
+
